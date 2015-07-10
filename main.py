c460af18a6745cd621d3cf83006fabf0c2aeb6e4
eon qee <ramazanbeg@gmail.com>
2015-07-10 21:36:16 +0300

    faster

--- main.py
+++ main.py
@@ -23,12 +23,9 @@ else:
 all_members = []
 for i in range(num_iters):
     url_members = 'https://api.vk.com/method/groups.getMembers?group_id={0}&offset={1}&fields={2}'.format(group_id, offset, fields)
+    response_url_members = urllib.urlopen(url_members)
+    members = json.loads(response_url_members.read())['response']['users']
     for j in range(max_offset-1):
-        response_url_members = urllib.urlopen(url_members)
-        members = json.loads(response_url_members.read())['response']['users'][j]
-        print j, members.get('university_name')
+        print j, members[j].get('university_name')
     offset+=max_offset
-    #all_members.extend(members)
     #print members
-
-#print(len(all_members), all_members)
